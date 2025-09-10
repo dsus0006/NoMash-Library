2e2b0b627d0ebf1f47f23ec601089d4e41dc7f21
Dominica Susanto <dsus0006@student.monash.edu>
2025-09-10 16:37:28 +1000

    Added week 6 and week 7 Firebase exercises

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import router from './router'
 
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
 // import DataTable from 'primevue/datatable'
 // import Column from 'primevue/Column'
 
@@ -17,5 +21,22 @@ app.use(router)
 
 // app.component('DataTable', DataTable)
 // app.component('Column', Column)
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0F44tXIec4pu-xlpt-PLHUsjYEPGR75o",
+  authDomain: "week7-dsusanto.firebaseapp.com",
+  projectId: "week7-dsusanto",
+  storageBucket: "week7-dsusanto.firebasestorage.app",
+  messagingSenderId: "162324637205",
+  appId: "1:162324637205:web:99148aa6fcf0e7acf0d01c"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 app.mount('#app')
